66a5845c6769edd9312354d413988236aec41eed
Wilson Kilonzo <mutidawilz@gmail.com>
2025-10-06 06:44:15 +0300

    Run unit tests for phone formats

--- backend/app/helpers/regex_helper.rb
+++ backend/app/helpers/regex_helper.rb
@@ -1,4 +1,5 @@
 module RegexHelper
+  # email_regex
   def email_format(email)
     email = email.to_s.gsub(/\s+/, '').downcase
     unless email.match?(/\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}\z/i)
@@ -6,4 +7,20 @@ module RegexHelper
     end
     email
   end
+
+  # phone_regex
+  def phone_format(phone)
+    phone = phone.to_s.gsub(/\s+/, '')
+    if phone.match?(/\A01\d{8}\z/)
+      phone = phone.sub(/\A01/, '2541')
+    elsif phone.match?(/\A07\d{8}\z/)
+      phone = phone.sub(/\A07/, '2547')
+    elsif phone.match?(/\A(2541|2547)\d{8}\z/)
+      phone = phone
+    else
+      raise 'Invalid Phone Format!'
+    end
+    phone
+  end
+
 end
--- backend/spec/helpers/regex_helper_spec.rb
+++ backend/spec/helpers/regex_helper_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe RegexHelper, type: :helper do
     end
 
   end
+
+  describe "#phone_format" do
+    it "normalized phone when valid" do
+      result = helper.phone_format(" 2541 00990099")
+      expect(result).to eq("254100990099")
+    end
+
+    it "returns an error if phpne is invalid" do
+      expect { helper.phone_format("254698989009") }.to raise_error("Invalid Phone Format!")
+    end
+
+    it "returns an error if phone is null" do
+      expect { helper.phone_format("") }.to raise_error("Invalid Phone Format!")
+    end
+
+    it "converts 01 prefix to 2541" do
+      result = helper.phone_format("0198989898")
+      expect(result).to eq("254198989898")
+    end
+
+    it "converts 07 prefix to 2547" do
+      old = helper.phone_format("0791738417")
+      expect(old).to eq("254791738417")
+    end
+
+    it "returns an error on non-digits" do
+      expect { helper.phone_format("0798i8u765") }.to raise_error("Invalid Phone Format!")
+    end
+  end
 end
